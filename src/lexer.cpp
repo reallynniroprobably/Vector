f8fa73ddbb0feb18432114060ca8040f1d99e05c
cairo <cairodevv@gmail.com>
2026-03-30 14:25:38 +1100

    Finished lexer

---
+++ src/lexer.cpp
@@ -0,0 +1,78 @@
+#include "lexer.h"
+
+// Helper to avoid repetition
+static void pushNewToken(std::vector<Token>& tokens) {
+    // Only push a new token if the current one isn't empty
+    if (!tokens.back().value.empty()) {
+        tokens.emplace_back();
+    }
+}
+
+LexedFile lexFile(const std::string& fileContents) {
+    std::vector<Token> lexedFile = {Token()};
+    bool isComment = false;
+    bool inString  = false;
+    bool inChar    = false;
+
+    for (const char c : fileContents) {
+        switch (c) {
+            case '"':
+                if (!isComment) {
+                    if (inChar) {
+                        lexedFile.back().value.push_back(c);
+                    } else {
+                        inString = !inString;
+                        lexedFile.back().value.push_back(c);
+                        if (!inString) pushNewToken(lexedFile); // closing quote = end of string token
+                    }
+                }
+                break;
+            case '\'':
+                if (!isComment) {
+                    if (inString) {
+                        lexedFile.back().value.push_back(c);
+                    } else {
+                        inChar = !inChar;
+                        lexedFile.back().value.push_back(c);
+                        if (!inChar) pushNewToken(lexedFile); // closing quote = end of char token
+                    }
+                }
+                break;
+            case ' ':
+            case '\t':
+                if (inString || inChar) {
+                    lexedFile.back().value.push_back(c);
+                } else {
+                    pushNewToken(lexedFile); // whitespace = token boundary
+                }
+                break;
+            case '\n':
+                if (inString || inChar) {
+                    lexedFile.back().value.push_back(c);
+                } else {
+                    pushNewToken(lexedFile); // newline = token boundary
+                    isComment = false;
+                }
+                break;
+            case '#':
+                if (inString || inChar) {
+                    lexedFile.back().value.push_back(c);
+                } else if (!isComment) {
+                    isComment = true;
+                }
+                break;
+            default:
+                if (!isComment) {
+                    lexedFile.back().value.push_back(c);
+                }
+                break;
+        }
+    }
+
+    // Remove trailing empty token if present
+    if (!lexedFile.empty() && lexedFile.back().value.empty()) {
+        lexedFile.pop_back();
+    }
+
+    return LexedFile(lexedFile);
+}
---
+++ src/lexer.h
@@ -0,0 +1,79 @@
+#ifndef LEXER
+#define LEXER
+#include <string>
+#include <vector>
+enum TokenType {
+    KEYWORD,
+    OPERATOR,
+    DELIMITER,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    CHARACTER_LITERAL,
+    BOOLEAN_LITERAL
+};
+
+enum Tokens {
+    IF,
+    FOR,
+    WHILE,
+    ELSE,
+    SWITCH,
+    CASE,
+    DO,
+    RETURN,
+    BREAK,
+    CONTINUE,
+    CLASS,
+    OBJECT,
+    THIS,
+    PUBLIC,
+    PRIVATE,
+    PROTECTED,
+    ENUM,
+    VOID,
+    CONST,
+    HEADER,
+    PLUS,
+    HYPHEN_MINUS,
+    ASTERISK,
+    SOLIDUS,
+    AMPERSAND,
+    DOUBLE_PLUS,
+    DOUBLE_HYPHEN_MINUS,
+    EQUAL,
+    NOT_EQUAL,
+    LESS_THAN,
+    GREATER_THAN,
+    LESS_THAN_OR_EQUAL,
+    GREATER_THAN_OR_EQUAL,
+    ASSIGN,
+    ADD_AND_ASSIGN,
+    SUBTRACT_AND_ASSIGN,
+    AND,
+    OR,
+    NOT,
+    PERIOD,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS,
+    LEFT_SQUARE_BRACKET,
+    RIGHT_SQUARE_BRACKET,
+    LEFT_CURLY_BRACKET,
+    RIGHT_CURLY_BRACKET,
+    SEMICOLON,
+    COLON,
+    COMMA,
+};
+struct Token {
+    std::string value;
+    explicit Token(std::string value = "") : value(std::move(value)) {}
+};
+
+struct LexedFile {
+    std::vector<Token> tokens;
+    explicit LexedFile(std::vector<Token> tokens) : tokens(std::move(tokens)) {}
+};
+
+LexedFile lexFile(const std::string &fileContents);
+
+#endif //LEXER
